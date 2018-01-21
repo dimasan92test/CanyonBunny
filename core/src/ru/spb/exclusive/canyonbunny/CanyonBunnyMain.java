443e0cd4fcef32d418c6cdb3a2f84efbca4b1c1c
dimasan92 <dimasan92@gmail.com>
2018-01-21 05:20:51 +0300

    добавлена инициализация классов для управления и прорисовки игрового мира, переданы им вызовы на обновление и прорисовку игрвого мира, а также на очищение игровых ресурсов добавлены вызовы паузы/возобновления для Android

--- core/src/ru/spb/exclusive/canyonbunny/CanyonBunnyMain.java
+++ core/src/ru/spb/exclusive/canyonbunny/CanyonBunnyMain.java
@@ -1,6 +1,12 @@
 package ru.spb.exclusive.canyonbunny;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+import ru.spb.exclusive.canyonbunny.game.WorldController;
+import ru.spb.exclusive.canyonbunny.game.WorldRenderer;
 
 public class CanyonBunnyMain implements ApplicationListener {
     private static final String TAG = CanyonBunnyMain.class.getName();
@@ -8,33 +14,53 @@ public class CanyonBunnyMain implements ApplicationListener {
     private WorldController worldController;
     private WorldRenderer worldRenderer;
 
+    private boolean paused;
+
     @Override
     public void create() {
+        // Set Libgdx log level to DEBUG
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+        // Initialize controller and renderer
+        worldController = new WorldController();
+        worldRenderer = new WorldRenderer(worldController);
 
+        // Game world is active on start
+        paused = false;
     }
 
     @Override
     public void render() {
-
+        // Do not update game world when paused.
+        if (!paused) {
+            // Update game world by the time that has passed
+            // since last rendered frame.
+            worldController.update(Gdx.graphics.getDeltaTime());
+        }
+        // Sets the clear screen color to: Cornflower Blue
+        Gdx.gl.glClearColor(100 / 255.0f, 149 / 255.0f, 237 / 255.0f, 255 / 255.0f);
+        // Clears the screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        // Render game world to screen
+        worldRenderer.render();
     }
 
     @Override
     public void resize(int width, int height) {
-        
+        worldRenderer.resize(width, height);
     }
 
     @Override
     public void pause() {
-
+        paused = true;
     }
 
     @Override
     public void resume() {
-
+        paused = false;
     }
 
     @Override
     public void dispose() {
-
+        worldRenderer.dispose();
     }
 }
