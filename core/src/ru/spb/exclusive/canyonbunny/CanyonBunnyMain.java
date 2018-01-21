62a6135fa10de36028203f7a24b219cb6c18a26f
dimasan92 <dimasan92@gmail.com>
2018-01-21 04:48:34 +0300

    Добавление главного класса игры с методами-заглушками

---
+++ core/src/ru/spb/exclusive/canyonbunny/CanyonBunnyMain.java
@@ -0,0 +1,40 @@
+package ru.spb.exclusive.canyonbunny;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class CanyonBunnyMain implements ApplicationListener {
+    private static final String TAG = CanyonBunnyMain.class.getName();
+
+    private WorldController worldController;
+    private WorldRenderer worldRenderer;
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
