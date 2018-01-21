c22e6e49e3a5f076c04784f9b6d4c0494acc7617
dimasan92 <dimasan92@gmail.com>
2018-01-22 01:00:07 +0300

    произведена инициализация загрузки ассетов через синглтон Assets

--- core/src/ru/spb/exclusive/canyonbunny/CanyonBunnyMain.java
+++ core/src/ru/spb/exclusive/canyonbunny/CanyonBunnyMain.java
@@ -3,8 +3,10 @@ package ru.spb.exclusive.canyonbunny;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 
+import ru.spb.exclusive.canyonbunny.game.Assets;
 import ru.spb.exclusive.canyonbunny.game.WorldController;
 import ru.spb.exclusive.canyonbunny.game.WorldRenderer;
 
@@ -20,6 +22,8 @@ public class CanyonBunnyMain implements ApplicationListener {
     public void create() {
         // Set Libgdx log level to DEBUG
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
+        // Load assets
+        Assets.instance.init(new AssetManager());
         // Initialize controller and renderer
         worldController = new WorldController();
         worldRenderer = new WorldRenderer(worldController);
@@ -62,5 +66,6 @@ public class CanyonBunnyMain implements ApplicationListener {
     @Override
     public void dispose() {
         worldRenderer.dispose();
+        Assets.instance.dispose();
     }
 }
