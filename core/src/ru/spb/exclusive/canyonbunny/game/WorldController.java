ac30f24fa61a74b5dc49d8c3f1a393bc984f1c38
dimasan92 <dimasan92@gmail.com>
2018-01-21 04:54:36 +0300

    создание базовой версии класса для управления игровым миром

---
+++ core/src/ru/spb/exclusive/canyonbunny/game/WorldController.java
@@ -0,0 +1,12 @@
+package ru.spb.exclusive.canyonbunny.game;
+
+public class WorldController {
+    private static final String TAG = WorldController.class.getName();
+
+    public WorldController() {
+    }
+
+    private void init(){}
+
+    private void update(float deltaTime){}
+}
