fe54a05fb3250c1623eaded824735aaf1b209c8c
dimasan92 <dimasan92@gmail.com>
2018-01-21 04:49:56 +0300

    класс для запуска игры на десктопе

--- desktop/src/ru/spb/exclusive/canyonbunny/desktop/DesktopLauncher.java
+++ desktop/src/ru/spb/exclusive/canyonbunny/desktop/DesktopLauncher.java
@@ -3,6 +3,8 @@ package ru.spb.exclusive.canyonbunny.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+import ru.spb.exclusive.canyonbunny.CanyonBunnyMain;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
