d8645ac3b7e0338023043fc3ad104d1fc187f4b4
dimasan92 <dimasan92@gmail.com>
2018-01-21 16:36:55 +0300

    добавление и инициализация CameraHelper. При помощи Enter позволяет привязать или отвзязать камеру к текущему объекту

--- core/src/ru/spb/exclusive/canyonbunny/game/WorldController.java
+++ core/src/ru/spb/exclusive/canyonbunny/game/WorldController.java
@@ -27,6 +27,7 @@ public class WorldController extends InputAdapter {
 
     private void init(){
         Gdx.input.setInputProcessor(this);
+        cameraHelper = new CameraHelper();
         initTestObjects();
     }
 
@@ -75,6 +76,7 @@ public class WorldController extends InputAdapter {
     public void update(float deltaTime){
         handleDebugInput(deltaTime);
         updateTestObjects(deltaTime);
+        cameraHelper.update(deltaTime);
     }
 
     private void updateTestObjects (float deltaTime) {
@@ -113,7 +115,17 @@ public class WorldController extends InputAdapter {
         // Select next sprite
         else if (keycode == Keys.SPACE) {
             selectedSprite = (selectedSprite + 1) % testSprites.length;
+            // Update camera's target to follow the currently
+            // selected sprite
+            if (cameraHelper.hasTarget()) {
+                cameraHelper.setTarget(testSprites[selectedSprite]);
+            }
             Gdx.app.debug(TAG, "Sprite #" + selectedSprite + " selected");
+        }
+        // Toggle camera follow
+        else if (keycode == Keys.ENTER) {
+            cameraHelper.setTarget(cameraHelper.hasTarget() ? null : testSprites[selectedSprite]);
+            Gdx.app.debug(TAG, "Camera follow enabled: " + cameraHelper.hasTarget());
         }
         return  false;
     }
