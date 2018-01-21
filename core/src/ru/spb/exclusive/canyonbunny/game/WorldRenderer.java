1d39902a412278efb549632f726de8d862a655e6
dimasan92 <dimasan92@gmail.com>
2018-01-21 15:37:54 +0300

    прорисовка тестовых объектов(5 спрайтов), созданных в worldController. Инициализация камеры в соответствии с областью просмотра в метрах и расчет новой области просмотра при изменении размера экрана

--- core/src/ru/spb/exclusive/canyonbunny/game/WorldRenderer.java
+++ core/src/ru/spb/exclusive/canyonbunny/game/WorldRenderer.java
@@ -1,8 +1,11 @@
 package ru.spb.exclusive.canyonbunny.game;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
+
+import ru.spb.exclusive.canyonbunny.util.Constants;
 
 public class WorldRenderer implements Disposable {
     private OrthographicCamera camera;
@@ -10,16 +13,37 @@ public class WorldRenderer implements Disposable {
     private WorldController worldController;
 
     public WorldRenderer(WorldController worldController) {
+        this.worldController = worldController;
+        init();
     }
 
-    private void init(){}
+    private void init(){
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+        camera.position.set(0, 0, 0);
+        camera.update();
+    }
 
-    public void render(){}
+    public void render(){
+        renderTestObjects();
+    }
 
-    public void resize(int width, int height){}
+    private void renderTestObjects(){
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        for (Sprite sprite : worldController.testSprites) {
+            sprite.draw(batch);
+        }
+        batch.end();
+    }
+
+    public void resize(int width, int height){
+        camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) * width;
+        camera.update();
+    }
 
     @Override
     public void dispose() {
-        
+        batch.dispose();
     }
 }
