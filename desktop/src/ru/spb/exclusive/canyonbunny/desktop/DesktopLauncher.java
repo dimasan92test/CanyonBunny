596269bd217826522cc1ffa2db253b81be1a878c
dimasan92 <dimasan92@gmail.com>
2018-01-21 04:21:00 +0300

    настройка проекта и загрузка в репозиторий

---
+++ desktop/src/ru/spb/exclusive/canyonbunny/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package ru.spb.exclusive.canyonbunny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new CanyonBunnyMain(), config);
+	}
+}
