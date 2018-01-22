dba306e6129d1c5622dd2631aaf8933eca8a09b2
dimasan92 <dimasan92@gmail.com>
2018-01-23 00:24:58 +0300

    изменение типа отслеживаемого объекта

--- core/src/ru/spb/exclusive/canyonbunny/util/CameraHelper.java
+++ core/src/ru/spb/exclusive/canyonbunny/util/CameraHelper.java
@@ -1,9 +1,10 @@
 package ru.spb.exclusive.canyonbunny.util;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+
+import ru.spb.exclusive.canyonbunny.game.objects.AbstractGameObject;
 
 public class CameraHelper {
     private static final String TAG = CameraHelper.class.getName();
@@ -13,7 +14,7 @@ public class CameraHelper {
 
     private Vector2 position;
     private float zoom;
-    private Sprite target;
+    private AbstractGameObject target;
 
     public CameraHelper () {
         position = new Vector2();
@@ -23,8 +24,8 @@ public class CameraHelper {
     public void update (float deltaTime) {
         if (!hasTarget()) return;
 
-        position.x = target.getX() + target.getOriginX();
-        position.y = target.getY() + target.getOriginY();
+        position.x = target.position.x + target.origin.x;
+        position.y = target.position.y + target.origin.y;
     }
 
     public void setPosition (float x, float y) {
@@ -47,11 +48,11 @@ public class CameraHelper {
         return zoom;
     }
 
-    public void setTarget (Sprite target) {
+    public void setTarget (AbstractGameObject target) {
         this.target = target;
     }
 
-    public Sprite getTarget () {
+    public AbstractGameObject getTarget () {
         return target;
     }
 
@@ -59,7 +60,7 @@ public class CameraHelper {
         return target != null;
     }
 
-    public boolean hasTarget (Sprite target) {
+    public boolean hasTarget (AbstractGameObject target) {
         return hasTarget() && this.target.equals(target);
     }
 
