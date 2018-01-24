f348726acaeaf601db277fd67c974aca556e1662
dimasan92 <dimasan92@gmail.com>
2018-01-25 01:44:26 +0300

    код из главного класса перенесен в GameScreen служит для запуска игры

--- core/src/ru/spb/exclusive/canyonbunny/CanyonBunnyMain.java
+++ core/src/ru/spb/exclusive/canyonbunny/CanyonBunnyMain.java
@@ -1,72 +1,25 @@
 package ru.spb.exclusive.canyonbunny;
 
 import com.badlogic.gdx.Application;
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.GL20;
 
 import ru.spb.exclusive.canyonbunny.game.Assets;
-import ru.spb.exclusive.canyonbunny.game.WorldController;
-import ru.spb.exclusive.canyonbunny.game.WorldRenderer;
+import ru.spb.exclusive.canyonbunny.screens.MenuScreen;
 
-public class CanyonBunnyMain implements ApplicationListener {
+public class CanyonBunnyMain extends Game {
     private static final String TAG = CanyonBunnyMain.class.getName();
 
-    private WorldController worldController;
-    private WorldRenderer worldRenderer;
-
-    private boolean paused;
-
     @Override
-    public void create() {
-        // Set Libgdx log level to DEBUG
+    public void create () {
+        // Set Libgdx log level
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
         // Load assets
         Assets.instance.init(new AssetManager());
-        // Initialize controller and renderer
-        worldController = new WorldController();
-        worldRenderer = new WorldRenderer(worldController);
 
-        // Game world is active on start
-        paused = false;
-    }
-
-    @Override
-    public void render() {
-        // Do not update game world when paused.
-        if (!paused) {
-            // Update game world by the time that has passed
-            // since last rendered frame.
-            worldController.update(Gdx.graphics.getDeltaTime());
-        }
-        // Sets the clear screen color to: Cornflower Blue
-        Gdx.gl.glClearColor(100 / 255.0f, 149 / 255.0f, 237 / 255.0f, 255 / 255.0f);
-        // Clears the screen
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        // Render game world to screen
-        worldRenderer.render();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        worldRenderer.resize(width, height);
-    }
-
-    @Override
-    public void pause() {
-        paused = true;
-    }
-
-    @Override
-    public void resume() {
-        Assets.instance.init(new AssetManager());
-        paused = false;
-    }
-
-    @Override
-    public void dispose() {
-        worldRenderer.dispose();
-        Assets.instance.dispose();
+        // Start game at menu screen
+        setScreen(new MenuScreen(this));
     }
 }
